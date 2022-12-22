4ba4cd32e7d9bab41a5046291da4f6d0aeda283a
Tom Staples <staples.2007@yahoo.co.uk>
2022-12-22 14:33:47 +0000

    Initial pages setup (CreateQuiz & Quiz)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from './components/Header';
+import CreateQuiz from './pages/CreateQuiz';
+import Quiz from './pages/Quiz';
 
 const App = () => {
 	return (
---
+++ src/pages/CreateQuiz/CreateQuiz.js
@@ -0,0 +1,9 @@
+const CreateQuiz = () => {
+	return (
+		<div>
+			<h1>Create Quiz Page</h1>
+		</div>
+	);
+};
+
+export default CreateQuiz;
