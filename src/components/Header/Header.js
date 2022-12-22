dcdd6d839af9a3e44c043be78f4f3f83c0cde995
Tom Staples <staples.2007@yahoo.co.uk>
2022-12-22 17:08:45 +0000

    Header component refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,8 @@ const Header = () => {
 	return (
 		<header className='bg-green-300 text-center h-20'>
 			<h1 className='font-title pt-4 text-4xl'>
-				Quizzical <span class='material-symbols-outlined text-4xl'>quiz</span>
+				Quizzical{' '}
+				<span className='material-symbols-outlined text-4xl'>quiz</span>
 			</h1>
 		</header>
 	);
