d5e1578ecf7b43651f6a8c5ba1ac92cafa44442d
Tom Staples <staples.2007@yahoo.co.uk>
2022-12-23 11:37:32 +0000

    Scrolling added for quiz list

--- src/pages/Home/QuizList/QuizList.js
+++ src/pages/Home/QuizList/QuizList.js
@@ -32,7 +32,7 @@ const QuizList = props => {
 	return (
 		<div className='flex flex-col items-center ml-2 w-1/2 bg-green-300 rounded p-4 shadow-xl'>
 			<h2 className='text-2xl underline font-title'>Quiz List</h2>
-			<ul className='flex flex-col w-full mt-4'>
+			<ul className='flex flex-col w-full my-4 h-96 overflow-auto'>
 				<li className='flex justify-between pl-2'>
 					<h3 className={headingStyle}>Questions</h3>
 					<h3 className={headingStyle}>Category</h3>
@@ -41,6 +41,15 @@ const QuizList = props => {
 				</li>
 				{list}
 			</ul>
+			<button
+				onClick={e => {
+					localStorage.clear();
+					props.setRounds([]);
+				}}
+				className='rounded bg-black text-white px-4 hover:bg-blue-300'
+			>
+				Delete All
+			</button>
 		</div>
 	);
 };
